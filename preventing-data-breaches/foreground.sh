e0fdc3f009bdf1dea5ff2e33c3269e610cabc676
Timothy Nguyen <nguyen.d.timothy@gmail.com>
2020-07-31 16:00:11 -0700

    hide executed commands in foreground script

--- preventing-data-breaches/foreground.sh
+++ preventing-data-breaches/foreground.sh
@@ -1,7 +1,6 @@
-{
-    while ! [ -f .get_envvars.sh ];
-    do
-        sleep 1
-    done
-    source .get_envvars.sh 
-} &> /dev/null
+set +x
+while ! [ -f .get_envvars.sh ];
+do
+    sleep 1
+done
+source .get_envvars.sh }
